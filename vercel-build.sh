f31cbb821206b54182c821802cff5e4819e5e9de
Gonuguntla Chetan <84891627+ChetanG111@users.noreply.github.com>
2025-11-20 08:56:05 +0530

    Update vercel-build.sh

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 npx prisma generate
+echo "=== LIST .prisma/client ==="
 ls -la node_modules/.prisma/client || true
+echo "=== LIST @prisma/client ==="
+ls -la node_modules/@prisma/client || true
 next build
